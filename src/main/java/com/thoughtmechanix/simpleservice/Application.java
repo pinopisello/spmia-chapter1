09438d300032ec30a90df6006ce5e72b9d8bb829
glocon <glocon@nike>
2017-03-14 17:11:12 -0700

    update 14/3/17

--- src/main/java/com/thoughtmechanix/simpleservice/Application.java
+++ src/main/java/com/thoughtmechanix/simpleservice/Application.java
@@ -2,14 +2,17 @@ package com.thoughtmechanix.simpleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @RestController
 @RequestMapping(value="hello")
+//@EnableCircuitBreaker      //Enable uso Hystrix-Ribbon
+//@EnableEurekaClient        //Registra service presso Eureka service discovery agent e indica che il service
+                           //usera' Service discovery to “lookup” the location of remote services
 public class Application {
 
     public static void main(String[] args) {
